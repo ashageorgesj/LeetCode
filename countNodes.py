87c7968a5f96b095af1a139d1c570e3f7d8c6bc7
Asha George <ashageor@gmail.com>
2020-06-23 16:32:14 -0700

    count nodes in a tree

---
+++ countNodes.py
@@ -0,0 +1,27 @@
+import collections
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def countNodes(self, root: TreeNode) -> int:
+        
+        q = collections.deque()
+        count = 0
+        if root:
+            count += 1
+            q.append(root)
+        while(len(q) > 0):
+            current = q.popleft()
+            #print(current.val,count)
+            if current.left:
+                count += 1
+                q.append(current.left)
+            if current.right:
+                count += 1
+                q.append(current.right)
+        
+        return count
+                
